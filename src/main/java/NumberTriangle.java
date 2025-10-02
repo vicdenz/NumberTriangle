f9685959ec41055029c27c6b701adf3c173af76b
vicdenz <david.daniliuc24@gmail.com>
2025-10-02 14:24:43 -0400

    Implemented loadTriangle method.

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -109,8 +111,7 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
+        List<NumberTriangle> prevRow = null;
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
@@ -118,11 +119,23 @@ public class NumberTriangle {
 
         String line = br.readLine();
         while (line != null) {
+            String[] numbers = line.split(" ");
+            List<NumberTriangle> currentRow = new ArrayList<>();
+            for (String number : numbers) {
+                currentRow.add(new NumberTriangle(Integer.parseInt(number)));
+            }
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            if (top == null) {
+                top = currentRow.get(0);
+            }
 
-            // TODO process the line
+            if (prevRow != null) {
+                for (int i = 0; i < prevRow.size(); i++) {
+                    prevRow.get(i).setLeft(currentRow.get(i));
+                    prevRow.get(i).setRight(currentRow.get(i+1));
+                }
+            }
+            prevRow = currentRow;
 
             //read the next line
             line = br.readLine();
